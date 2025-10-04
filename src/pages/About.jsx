98b5602f3dd300a624357258b02a42004bb01942
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-10-05 00:07:38 +0530

    profile card added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,9 @@ import Profile from "../assets/Profile.png";
 
 export default function About() {
   return (
-    <section className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-black via-[#120024] to-gray-900 py-12 px-4">
+    <section className="min-h-screen w-full flex items-center justify-start bg-gradient-to-br from-black via-[#120024] to-gray-900 py-12 px-4">
       <ProfileCard
+        className="ml-10"
         name="Lungsom Lamnio"
         title="Full Stack Developer"
         handle="lungsomlamnio"
